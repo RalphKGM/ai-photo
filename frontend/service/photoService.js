839aac53e1695b535624f56774ca27c93c1b8c18
alex <alexanderjannespia0127@gmail.com>
2026-02-22 23:49:29 +0800

    chore (frontend): add photoService

---
+++ frontend/service/photoService.js
@@ -0,0 +1,75 @@
+import * as MediaLibrary from 'expo-media-library';
+import * as ImagePicker from 'expo-image-picker';
+import { getSession } from './auth/authService';
+import { API_URL } from 'config/api';
+
+export const takePhoto = async () => {
+    const { status: cameraStatus } = await ImagePicker.requestCameraPermissionsAsync();
+    const { status: mediaStatus } = await MediaLibrary.requestPermissionsAsync();
+
+    if (cameraStatus !== 'granted' || mediaStatus !== 'granted') {
+        Alert.alert('Permission Denied', 'Camera and Library access are required.');
+        return;
+    }
+
+    const result = await ImagePicker.launchCameraAsync({
+        quality: 1,
+    });
+
+    if (result.canceled) return;
+
+    await MediaLibrary.createAssetAsync(result.assets[0].uri);
+
+    const token = await getSession();
+
+    const formData = new FormData();
+    formData.append('image', {
+        uri: result.assets[0].uri,
+        name: 'photo.jpg',
+        type: 'image/jpeg',
+    });
+
+    try {
+        const response = await fetch(`${API_URL}/api/image`, {
+            method: 'POST',
+            body: formData,
+            headers: { 
+                'Content-Type': 'multipart/form-data',
+                'Authorization': `Bearer ${token}`
+            },
+        });
+
+        const data = await response.json();
+        return result.assets[0].uri;
+    } catch (error) {
+        console.error("Upload failed", error);
+        return null;
+    }
+
+}
+
+export const getPhotos = async () => {
+  try {
+    const token = await getSession();
+
+    const response = await fetch(`${API_URL}/api/search`, {
+      method: 'POST',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({ query: '' }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      return data.result;
+    } else {
+      throw new Error(data.error || 'Failed to load photos');
+    }
+  } catch (error) {
+    console.error("getPhotos Service Error:", error.message);
+    throw error; 
+  }
+};
